2def7d768ac28ee4ed8b33be55458d5540df455d
Marijn Ploeg <marijnploeg@gmail.com>
2021-05-06 15:52:12 +0200

    repair form

---
+++ src/pages/createRepairPage/CreateRepairForm.js
@@ -0,0 +1,10 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+
+const CreateRepair = () => {
+    return (
+        <p>create repair</p>
+    )
+}
+
+export default CreateRepair();
